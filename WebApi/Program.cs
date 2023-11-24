3be89cb20dec97097a975a840a7187d285e14953
salihdurus <durussalih@gmail.com>
2023-11-24 16:25:24 +0300

    pagination added

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -35,6 +35,7 @@ builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureLoggerService();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.ConfigureActionFilters();
+builder.Services.ConfigureCors();
 
 var app = builder.Build();
 var logger = app.Services.GetRequiredService<ILoggerService>();
@@ -51,6 +52,8 @@ if (app.Environment.IsProduction())
     app.UseHsts();
 }
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
